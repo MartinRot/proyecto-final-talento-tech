f0082f7e13f4efb8acbf90a3cfba59c0d36e16ba
MartinRot <martin_rot@hotmail.com>
2025-07-09 17:54:24 -0300

    fix firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -13,26 +13,6 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APP_ID,
 };
 
-// Your web app's Firebase configuration
-/* const firebaseConfig = {
-  apiKey: "AIzaSyB0Z-w4YRdKFobJqGTFyQjxkZdy3XtM0bA",
-  authDomain: "proyecto-final-talentotech.firebaseapp.com",
-  projectId: "proyecto-final-talentotech",
-  storageBucket: "proyecto-final-talentotech.firebasestorage.app",
-  messagingSenderId: "1020721642123",
-  appId: "1:1020721642123:web:d29382c3552081fa8e328e"
-  };
-
-  .env
-  FIREBASE_API_KEY=AIzaSyB0Z-w4YRdKFobJqGTFyQjxkZdy3XtM0bA
-  FIREBASE_AUTH_DOMAIN=proyecto-final-talentotech.firebaseapp.com
-  FIREBASE_PROJECT_ID=proyecto-final-talentotech
-  FIREBASE_STORAGE_BUCKET=proyecto-final-talentotech.firebasestorage.app
-  FIREBASE_MESSAGING_SENDER_ID=1020721642123
-  FIREBASE_APP_ID=1:1020721642123:web:d29382c3552081fa8e328e
-  JWT_SECRET_KEY=miclaveultrasecreta123456
- */
-
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
